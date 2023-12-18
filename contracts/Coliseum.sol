d9cac19c83fb69757b3262bc5929eb65ea98ffbd
sourav maji <majisourav255@gmail.com>
2023-12-18 11:30:45 +0530

    updated coliseum token

--- contracts/Coliseum.sol
+++ contracts/Coliseum.sol
@@ -5,16 +5,49 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Coliseum is ERC20, Ownable {
-    uint256 public maxSupply = 1000000000 * 10 ** decimals(); // 1 billion tokens
+    uint256 public maxSupply = 1000000000 * 10 ** decimals(); 
     uint256 public totalMinted;
+    bool public mintingPaused;
+    uint256 public maxMintPerWallet;
+    uint256 public maxMintPerTransaction;
 
-    constructor() ERC20("COLISEUM", "CMAX") {
+    mapping(address => uint256) public mintedPerWallet;
+
+    constructor(uint256 _maxMintPerWallet, uint256 _maxMintPerTransaction) ERC20("COLISEUM", "CMAX") {
         totalMinted = 0;
+        mintingPaused = false;
+        maxMintPerWallet = _maxMintPerWallet;
+        maxMintPerTransaction = _maxMintPerTransaction;
     }
 
-    function mint(address to, uint256 amount) public {
+    modifier mintingNotPaused() {
+        require(!mintingPaused, "Minting is paused");
+        _;
+    }
+
+    function setMaxMintPerWallet(uint256 _maxMint) external onlyOwner {
+        maxMintPerWallet = _maxMint;
+    }
+
+    function setMaxMintPerTransaction(uint256 _maxMint) external onlyOwner {
+        maxMintPerTransaction = _maxMint;
+    }
+
+    function pauseMinting() external onlyOwner {
+        mintingPaused = true;
+    }
+
+    function resumeMinting() external onlyOwner {
+        mintingPaused = false;
+    }
+
+    function mint(address to, uint256 amount) public mintingNotPaused {
         require(totalMinted + amount <= maxSupply, "Exceeds max supply");
+        require(mintedPerWallet[to] + amount <= maxMintPerWallet, "Exceeds max mint per wallet");
+        require(amount <= maxMintPerTransaction, "Exceeds max mint per transaction");
+        
         _mint(to, amount);
         totalMinted += amount;
+        mintedPerWallet[to] += amount;
     }
 }
